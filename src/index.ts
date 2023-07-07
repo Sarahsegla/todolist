3abc6026f1a2c73daace551b186f7ec7d5bd771b
Sarahsegla <sarahsegla@icloud.com>
2023-07-07 17:05:07 +0100

    inside addlist  todo function. our list is apennding

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,38 @@ e.preventDefault()
 // optional chaining
 if (myInput?.value == "" || myInput?.value == null) return 
 
-const task = {
+const newToDo = {
   // the id is uuidV4 that we have imported at the top
-  id: uuidV4()
+  id: uuidV4(),
+  title: myInput.value,
+  completed: false,
+  whenCreated: new Date(),
 }
 
+addListItem(newToDo)
 
-
-
-myInput.value
 } )
+
+function addListItem(toDo: {
+  id: string,
+  title: string,
+  completed: boolean,
+  whenCreated: Date,
+
+  
+
+}) {
+  // creating the element li (list) and putting it in item variable
+  const item = document.createElement('li')
+  const label = document.createElement('label')
+  const tickbox = document.createElement('input')
+  // type of the boc is checkbox
+  tickbox.type = 'checkbox'
+ // inside the label should be tickox and my to do
+  label.append(tickbox, toDo.title)
+ // inside item (li) should be my label
+  item.append(label)
+  // if theres anything on the list. show it
+  list?.append(item)
+
+}
